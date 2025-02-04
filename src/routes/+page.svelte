ccd22f8f8ec42b4942c928b029d39baf746bd87a
WithcenterDev6 <withcenterdev6@WithcenterDev6s-iMac.local>
2025-02-04 09:43:36 +0800

    practice commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    let message = $state(null);
+</script>
+
+<h1>Training Project</h1>
+
+<container>
+    <textarea id="message" bind:value={message}></textarea>
+</container>
+
+<h1>{message}</h1>
+<h1>{message}</h1>
+
+<style>
+    #message {
+        resize: none;
+        border-radius: 20px;
+        text-align: center;
+    }
+</style>
